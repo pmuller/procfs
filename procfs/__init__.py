39e4737840784eeb075ba20e4104d46fc2775c24
Philippe Muller <philippe.muller@gmail.com>
2012-04-29 09:41:49 -0700

    0.1.2

--- procfs/__init__.py
+++ procfs/__init__.py
@@ -2,4 +2,4 @@
 
 from procfs.core import Proc
 
-__version__ = '0.1.0'
+__version__ = '0.1.2'
--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup, find_packages
 
-VERSION = [l for l in open('procfs/__init__.py')
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+VERSION = [l for l in read('procfs/__init__.py').splitlines()
            if l.startswith('__version__ =')][0].split("'")[1]
-LONG_DESCRIPTION = open('README.rst').read()
 
 setup(
     name='procfs',
     version=VERSION,
     packages=find_packages(),
     author='Philippe Muller',
+    author_email='philippe.muller@gmail.com',
     description='Python API for the Linux /proc virtual filesystem',
-    long_description=LONG_DESCRIPTION,
+    long_description=read('README.rst'),
     license='BSD',
     keywords='linux proc procfs system kernel',
     url='https://github.com/pmuller/procfs',
