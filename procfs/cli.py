921a368c1f30265d5ce31f1944bc079471b8de40
Robert Xu <robxu9@gmail.com>
2014-07-22 13:25:10 -0400

    Modify not to use eval()

--- procfs/cli.py
+++ procfs/cli.py
@@ -1,11 +1,15 @@
 import argparse
 import json
 import os
+import sys
+import string
 
-from procfs import Proc 
+from procfs import Proc
 from procfs.core import ProcDirectory
 from procfs.core import ProcessFile
 from procfs.core import File
+
+from procfs.exceptions import DoesNotExist
 
 
 def run():
@@ -13,16 +17,44 @@ def run():
 	parser.add_argument('path', metavar='PATH', type=str, nargs='?', default="", help='procfs path')
 	args = parser.parse_args()
 
-	proc = Proc()
+	if len(args.path) == 0:
+		sys.stderr.write('no arguments specified - run with -h for help.\n')
+		return
 
-	# this is the cheap way, but shouldn't use eval
-	# this also doesn't work for numeric indexes like cpuinfo/0/cpu_family
-	obj = eval("proc."+args.path.replace("/", "."))
-	if callable(obj):
-		obj = obj()
-	try:
-		print json.dumps(obj)
-	except TypeError, e:
-		print obj.__class__
-		raise e
+	obj = Proc()
+	path = args.path
+	failed = -1
+	
+	for (k, v) in enumerate(path.split('/')):
+		try:
+			obj = obj.__getitem__(v)
+		except DoesNotExist:
+			failed = k
+			break
+		except AttributeError:
+			failed = k
+			break
+	
+	obj = obj()
+	
+	if failed != -1:
+		for (k, v) in enumerate(path.split('/')):
+			if k < failed:
+				continue
+			try:
+				obj = obj.__getattr__(v)
+			except KeyError:
+				try:
+					obj = obj.__getattr__(int(v))
+				except ValueError:
+					sys.stderr.write('failed to get specific path; getting parent(s)\n')
+					break
+				except AttributeError:
+					sys.stderr.write('failed to get specific path; getting parent(s)\n')
+					break
+			except AttributeError:
+				sys.stderr.write('failed to get specific path; getting parent(s)\n')
+				break
+	
+	print obj
 	return
