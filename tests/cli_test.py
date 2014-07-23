77154a2df39dce2a93e46e2da66afe5cf4ed099e
Robert Xu <robxu9@gmail.com>
2014-07-23 14:37:56 -0400

    Add test for our find() method.
    Test with py.test.

---
+++ tests/cli_test.py
@@ -0,0 +1,27 @@
+import pytest
+
+import procfs
+from procfs import cli
+from procfs.exceptions import DoesNotExist
+
+@pytest.fixture
+def proc():
+    return None
+
+def test_proc_timedata(proc):
+    assert cli.find('1/stat', False)
+
+def test_proc_processes(proc):
+    assert cli.find('1', False)
+
+def test_proc_itemorattr(proc):
+    assert cli.find('loadavg/average/15', False)
+
+def test_proc_badfile(proc):
+    with pytest.raises(DoesNotExist):
+        assert cli.find('jbsdabfjasdbfsdafl', False)
+
+def test_proc_cannotlist(proc):
+    with pytest.raises(AttributeError):
+        assert cli.find('cpuinfo/0/cpu_family', True)
+
