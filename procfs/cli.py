2441672bcaf936521927e43f19a62c107f392046
Robert Xu <robxu9@gmail.com>
2014-07-22 20:39:40 -0400

    Always output JSON

--- procfs/cli.py
+++ procfs/cli.py
@@ -13,58 +13,64 @@ from procfs.exceptions import DoesNotExist
 
 
 def run():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('path', metavar='PATH', type=str, nargs='?', default="", help='procfs path')
-	parser.add_argument('-l', '--list', help='list all available keys', action='store_true')
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', metavar='PATH', type=str, nargs='?', default="", help='procfs path')
+    parser.add_argument('-l', '--list', help='list all available keys', action='store_true')
+    args = parser.parse_args()
 
-	obj = Proc()
-	path = args.path
-	failed = -1
+    obj = Proc()
+    path = args.path
+    failed = -1
 
-	for (k, v) in enumerate(path.split('/')):
-		try:
-			obj = obj.__getitem__(v)
-		except DoesNotExist:
-			failed = k
-			break
-		except AttributeError:
-			failed = k
-			break
+    for (k, v) in enumerate(path.split('/')):
+        try:
+            obj = obj.__getitem__(v)
+        except DoesNotExist:
+            failed = k
+            break
+        except AttributeError:
+            failed = k
+            break
 
-	if callable(obj):
-		obj = obj()
+    if callable(obj):
+        obj = obj()
 
-	if failed != -1:
-		for (k, v) in enumerate(path.split('/')):
-			if k < failed:
-				continue
-			try:
-				obj = obj.__getattr__(v)
-			except KeyError:
-				try:
-					obj = obj.__getattr__(int(v))
-				except ValueError:
-					sys.stderr.write('failed to get specific path; getting parent(s)\n')
-					break
-				except AttributeError:
-					sys.stderr.write('failed to get specific path; getting parent(s)\n')
-					break
-			except AttributeError:
-				sys.stderr.write('failed to get specific path; getting parent(s)\n')
-				break
+    if failed != -1:
+        for (k, v) in enumerate(path.split('/')):
+            if k < failed:
+                continue
+            try:
+                obj = obj.__getattr__(v)
+            except KeyError:
+                try:
+                    obj = obj.__getattr__(int(v))
+                except ValueError:
+                    sys.stderr.write('no such attribute %s\n' % v)
+                    sys.exit(1)
+                except AttributeError:
+                    sys.stderr.write('no such attribute %s\n' % v)
+                    sys.exit(1)
+            except AttributeError:
+                sys.stderr.write('no such attribute %s\n' % v)
+                sys.exit(1)
 
-	if args.list:
-		if isinstance(obj, dict):
-			print json.dumps(obj.keys())
-		elif isinstance(obj, ProcDirectory):
-			keys = []
-			for key in obj.__dir__():
-				keys.append(key)
-			print json.dumps(keys)
-		else:
-			sys.stderr.write('requested path does not hold a dictionary!\n')
-			sys.exit(1)
-	else:
-		print obj
-	return
+    if args.list:
+        if isinstance(obj, dict):
+            print json.dumps(obj.keys())
+        elif isinstance(obj, ProcDirectory):
+            keys = []
+            for key in obj.__dir__():
+                keys.append(key)
+            print json.dumps(keys)
+        else:
+            sys.stderr.write('requested path does not hold a dictionary!\n')
+            sys.exit(1)
+    else:
+        if isinstance(obj, ProcDirectory):
+            keys = []
+            for key in obj.__dir__():
+                keys.append(key)
+            print json.dumps(keys)
+        else:
+            print json.dumps(obj)
+    return
